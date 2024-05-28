7d26f5d029c205d7907c449ae271270e235ee0a1
LeeMinYeong <lmy1366@gmail.com>
2024-05-28 11:28:31 +0900

    first init

---
+++ src/main/java/com/booktory/booktoryserver/BookToryServerApplication.java
@@ -0,0 +1,13 @@
+package com.booktory.booktoryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookToryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookToryServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/booktory/booktoryserver/BookToryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booktory.booktoryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookToryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
