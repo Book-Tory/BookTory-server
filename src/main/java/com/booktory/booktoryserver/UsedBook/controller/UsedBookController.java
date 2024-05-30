ad70f546329a5914d335514f123f803c4714b1a1
Sung-Heee <praaa0220@gmail.com>
2024-05-30 17:16:30 +0900

    [feat] 검색한 책 조회 기능 #6

--- src/main/java/com/booktory/booktoryserver/UsedBook/controller/UsedBookController.java
+++ src/main/java/com/booktory/booktoryserver/UsedBook/controller/UsedBookController.java
@@ -6,31 +6,40 @@ import com.booktory.booktoryserver.common.CustomResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/used-books")
+@RequestMapping("/api/used-books")
 @RequiredArgsConstructor
 @Slf4j
 
 public class UsedBookController {
     private final UsedBookService usedBookService;
 
-    @GetMapping("/search-books")
+    // 책 전체 조회
+    @GetMapping("/books")
     public CustomResponse searchBooks(@RequestParam ("query") String query, @RequestParam ("display") int display) throws JsonProcessingException {
         List<BookDTO> bookInfoList = usedBookService.searchBooks(query, display);
 
-        if (bookInfoList == null) {
-            return CustomResponse.failure("조회 실패");
-        } else {
+        if (bookInfoList != null) {
             return CustomResponse.ok("조회 완료", bookInfoList);
+        } else {
+            return CustomResponse.failure("조회 실패");
         }
+    }
 
+    // 선택한 책 조회
+    @GetMapping("/book/{d_isbn}")
+    public CustomResponse getBookByIsbn (@PathVariable ("d_isbn") Long d_isbn) throws JsonProcessingException {
+        BookDTO bookInfo = usedBookService.getBookByIsbn(d_isbn);
+
+        if (bookInfo != null) {
+            return CustomResponse.ok("조회 성공", bookInfo);
+        } else {
+            return CustomResponse.failure("조회 실패");
+        }
     }
 
 }
--- src/main/java/com/booktory/booktoryserver/UsedBook/service/UsedBookService.java
+++ src/main/java/com/booktory/booktoryserver/UsedBook/service/UsedBookService.java
@@ -14,7 +14,9 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -27,18 +29,25 @@ public class UsedBookService {
     @Value("${X-Naver-Client-Secret}")
     private String client_secret;
 
-    public List<BookDTO> searchBooks(String query, int display) throws JsonProcessingException {
+    private URI buildUri (String path, Map<String, Object> queryParams) {
+        // 1. 기본 URI 설정
+
         // UriComponentsBuilder -> URI를 동적으로 생성하고 조작하는 데 사용
         // 스키마, 호스트, 경로, 쿼리 파라미터 등 다양한 URI 구성 요소를 쉽게 설정 가능
-        URI uri = UriComponentsBuilder
+        UriComponentsBuilder builder = UriComponentsBuilder
                 .fromUriString("https://openapi.naver.com")
-                .path("/v1/search/book.json")
-                .queryParam("query", query)
-                .queryParam("display", display)
+                .path(path);
+
+        // 2. queryParams 맵의 각 엔트리를 반복 -> queryParam 추가
+        queryParams.forEach(builder::queryParam);
+
+        return builder
                 .encode() // URI 인코딩
                 .build() // UriComponents 객체를 생성
                 .toUri(); // UriComponents 객체를 java.net.URI 객체로 변환
+    }
 
+    private ResponseEntity<String> sendRequest(URI uri) {
         // 요청 헤더 설정
         RequestEntity<Void> req = RequestEntity
                 .get(uri)
@@ -52,6 +61,18 @@ public class UsedBookService {
         // API 호출 및 응답 받기
         ResponseEntity<String> res = restTemplate.exchange(req, String.class);
 
+        return res;
+    }
+
+    public List<BookDTO> searchBooks(String query, int display) throws JsonProcessingException {
+        Map<String, Object> queryParams = new HashMap<>();
+        queryParams.put("query", query);
+        queryParams.put("display", display);
+
+        URI uri = buildUri("/v1/search/book.json", queryParams);
+
+        ResponseEntity<String> res = sendRequest(uri);
+
         ObjectMapper mapper = new ObjectMapper();
         BookResponseDTO bookResponseDTO = mapper.readValue(res.getBody(), BookResponseDTO.class);
 
@@ -60,4 +81,25 @@ public class UsedBookService {
 
         return bookInfoList;
     }
+
+
+
+    public BookDTO getBookByIsbn(Long d_isbn) throws JsonProcessingException {
+        Map<String, Object> queryParams = new HashMap<>();
+
+        queryParams.put("d_isbn", d_isbn);
+
+        URI uri = buildUri("/v1/search/book_adv.json", queryParams);
+
+        ResponseEntity<String> res = sendRequest(uri);
+
+        ObjectMapper mapper = new ObjectMapper();
+        BookResponseDTO bookResponseDTO = mapper.readValue(res.getBody(), BookResponseDTO.class);
+
+        BookDTO bookInfo = bookResponseDTO.getItems().get(0);
+
+        return bookInfo;
+    }
+
+
 }
