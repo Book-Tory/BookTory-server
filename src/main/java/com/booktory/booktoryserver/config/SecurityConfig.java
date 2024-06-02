f44a4157d2159f3b135b99752980835f4bac59ef
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-06-02 21:40:29 +0900

    [feat] security default setting #11

---
+++ src/main/java/com/booktory/booktoryserver/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.booktory.booktoryserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests((auth) -> {
+                    auth
+                            .anyRequest().permitAll();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
