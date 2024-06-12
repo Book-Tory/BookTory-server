eb59b880195ed43ce07afae1e5e5e524905dde36
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-06-12 15:30:39 +0900

    [release] git action docker1

--- src/main/java/com/booktory/booktoryserver/config/SecurityConfig.java
+++ src/main/java/com/booktory/booktoryserver/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.booktory.booktoryserver.config;
 import com.booktory.booktoryserver.Users.filter.JWTFilter;
 import com.booktory.booktoryserver.Users.filter.JWTUtil;
 import com.booktory.booktoryserver.Users.filter.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -38,6 +43,20 @@ public class SecurityConfig {
 
         http
                 .csrf((csrf) -> csrf.disable());
+
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L); //1시간
+                        return config;
+                    }
+                }));
 
         http
                 .formLogin((formLogin) -> formLogin.disable());
