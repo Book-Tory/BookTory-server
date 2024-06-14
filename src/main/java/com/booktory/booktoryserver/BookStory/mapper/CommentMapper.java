c625c21b1b3ff7c8f66bd20649335e521b606a84
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-06-14 14:41:16 +0900

    CI/CD 재배포-13

--- src/main/java/com/booktory/booktoryserver/BookStory/mapper/CommentMapper.java
+++ src/main/java/com/booktory/booktoryserver/BookStory/mapper/CommentMapper.java
@@ -2,9 +2,11 @@ package com.booktory.booktoryserver.BookStory.mapper;
 
 import com.booktory.booktoryserver.BookStory.domain.CommentEntity;
 import com.booktory.booktoryserver.BookStory.dto.response.CommentViewDTO;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
+@Mapper
 public interface CommentMapper {
     List<CommentEntity> getAllCommentsByStoryId(Long story_board_id);
     int createComment(CommentEntity commentEntity);
