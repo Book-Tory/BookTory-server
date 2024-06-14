70392b90b1fb1c048894098f64bde76ccc5f3582
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-06-15 02:50:50 +0900

    CI/CD 재배포 25

--- src/main/java/com/booktory/booktoryserver/config/SecurityConfig.java
+++ src/main/java/com/booktory/booktoryserver/config/SecurityConfig.java
@@ -102,7 +102,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://52.78.9.158:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "access", "refresh"));
