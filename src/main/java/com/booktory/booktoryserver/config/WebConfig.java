f4401bf4b6d5444cbbed18b15703978b5d5cd717
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-06-14 19:51:41 +0900

    CI/CD 재배포-21

--- src/main/java/com/booktory/booktoryserver/config/WebConfig.java
+++ src/main/java/com/booktory/booktoryserver/config/WebConfig.java
@@ -7,14 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://52.78.9.158:8282", "http://52.78.9.158:5173", "http://52.78.9.158:80")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Authorization", "Content-Type", "access", "refresh")
-                .exposedHeaders("access", "refresh")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173", "http://52.78.9.158:8282", "http://52.78.9.158:5173", "http://52.78.9.158:80")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("Authorization", "Content-Type", "access", "refresh")
+//                .exposedHeaders("access", "refresh")
+//                .allowCredentials(true)
+//                .maxAge(3600);
+//    }
 }
