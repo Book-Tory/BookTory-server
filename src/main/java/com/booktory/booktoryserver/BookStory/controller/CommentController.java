bdd1334d0b1319078beccb0e8a53cc81dddc5f82
sanghoon <psh950313@gmail.com>
2024-06-14 13:09:18 +0900

    "[feat] 독후감 게시판 댓글 기능 #40" 댓글 CRUD Controller 작성

--- src/main/java/com/booktory/booktoryserver/BookStory/controller/CommentController.java
+++ src/main/java/com/booktory/booktoryserver/BookStory/controller/CommentController.java
@@ -1,16 +1,14 @@
 package com.booktory.booktoryserver.BookStory.controller;
 
 
+import com.booktory.booktoryserver.BookStory.dto.request.CommentCreateDTO;
 import com.booktory.booktoryserver.BookStory.dto.response.CommentViewDTO;
 import com.booktory.booktoryserver.BookStory.service.CommentService;
 import com.booktory.booktoryserver.QnAComment.service.QnaCommentService;
 import com.booktory.booktoryserver.common.CustomResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +31,44 @@ public class CommentController {
         }
     }
 
+    @PostMapping("/{story_board_id}/comments")
+    public CustomResponse createComment(@PathVariable Long story_board_id, @RequestBody CommentCreateDTO commentCreateDTO){
+        commentCreateDTO.setBoardId(story_board_id);
+        boolean result = commentService.createComment(commentCreateDTO);
+        if(result){
+            return CustomResponse.ok("댓글 등록 성공", true);
+        }else{
+            return CustomResponse.failure("댓글 등록 실패");
+        }
+    }
 
+    @DeleteMapping("/comments/{comment_id}")
+    public CustomResponse deleteComment(@PathVariable Long comment_id) {
+        boolean result = commentService.deleteComment(comment_id);
+        if(result) {
+            return CustomResponse.ok("댓글 삭제 성공", true);
+        } else {
+            return CustomResponse.failure("댓글 삭제 실패");
+        }
+    }
+
+    @PutMapping("/comments/{comment_id}")
+    public CustomResponse updateComment(@PathVariable Long comment_id, @RequestBody CommentCreateDTO commentCreateDTO) {
+        boolean result = commentService.updateComment(comment_id, commentCreateDTO);
+        if(result) {
+            return CustomResponse.ok("댓글 수정 성공", true);
+        } else {
+            return CustomResponse.failure("댓글 수정 실패");
+        }
+    }
+
+    @GetMapping("/comments/{comment_id}")
+    public CustomResponse getCommentById(@PathVariable Long comment_id) {
+        CommentViewDTO comment = commentService.getCommentById(comment_id);
+        if(comment != null) {
+            return CustomResponse.ok("댓글 조회 성공", comment);
+        } else {
+            return CustomResponse.failure("댓글 조회 실패");
+        }
+    }
 }
