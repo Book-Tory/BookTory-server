5a4f75b8d609b1426ff951bbe3fae2fe77a16668
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-06-14 18:23:54 +0900

    CI/CD 재배포-19

--- src/main/java/com/booktory/booktoryserver/config/WebConfig.java
+++ src/main/java/com/booktory/booktoryserver/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://52.78.9.158:8282", "http://52.78.9.158:5173")
+                .allowedOrigins("http://localhost:5173", "http://52.78.9.158:8282", "http://52.78.9.158:5173", "http://52.78.9.158:80")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type", "access", "refresh")
                 .exposedHeaders("access", "refresh")
