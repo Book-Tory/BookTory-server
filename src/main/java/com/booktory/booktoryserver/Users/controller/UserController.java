dab65f26090c772803d15b9bcbc1bf04fc21ba2f
BaekSeungChan <106970129+BaekSeungChan@users.noreply.github.com>
2024-06-18 01:16:27 +0900

    #63 회원가입 추가 수정 작업

--- src/main/java/com/booktory/booktoryserver/Users/controller/UserController.java
+++ src/main/java/com/booktory/booktoryserver/Users/controller/UserController.java
@@ -6,6 +6,7 @@ import com.booktory.booktoryserver.Users.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +17,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public CustomResponse<String> register(@Valid @RequestBody UserRegisterDTO userRegisterDTO, BindingResult bindingResult) {
-
+    public CustomResponse<String> register(@Validated @RequestBody UserRegisterDTO userRegisterDTO, BindingResult bindingResult) {
 
         if(bindingResult.hasErrors()){
             return CustomResponse.failure(bindingResult.getAllErrors().get(0).getDefaultMessage());
