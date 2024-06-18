9321ce67353af2da331e283b543ccff6362b78a6
Sung-Heee <praaa0220@gmail.com>
2024-06-18 19:17:06 +0900

    [feat] 알림 불러오기 #71

---
+++ src/main/java/com/booktory/booktoryserver/Alram/domain/AlarmEntity.java
@@ -0,0 +1,32 @@
+package com.booktory.booktoryserver.Alram.domain;
+
+import com.booktory.booktoryserver.Chat.dto.ChatMessageDTO;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AlarmEntity {
+    private Long alarm_id;
+    private Long source_user_id;
+    private Long destination_user_id;
+    private Long entity_id;
+    private Long entity_type_id;
+    private Boolean is_read;
+    private Date create_date;
+
+    public static AlarmEntity toAlarmEntity(ChatMessageDTO chatMessage, Long receiverId) {
+        return AlarmEntity.builder()
+                .source_user_id(chatMessage.getSender_id())
+                .destination_user_id(receiverId)
+                .entity_id(chatMessage.getChat_message_id())
+                .entity_type_id(1L)
+                .is_read(false)
+                .build();
+    }
+}
